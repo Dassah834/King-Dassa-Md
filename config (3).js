0dba3bff446d4f8c18c53e1c8823bdc9d0a9bc8c
Dassah834 <dassagimhana64@gmail.com>
2024-09-19 16:13:02 +0530

    Add files via upload

---
+++ config (3).js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "TAkiRATC#QRBYKXUyIN-SmIeT32UVFJEAabsFINcyOVATHsMeAh0",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389",
+SUDO_NB: process.env.SUDO_NB || "94718913389",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true"
+};
+
